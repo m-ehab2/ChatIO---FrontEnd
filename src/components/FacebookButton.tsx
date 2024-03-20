4f6eda010f2bd70e4d330d538ddcbd9fb51b140c
Hassanmo99 <104209403+Hassanmo99@users.noreply.github.com>
2024-03-21 01:51:48 +0200

    modify: Facebook button styling

--- src/components/FacebookButton.tsx
+++ src/components/FacebookButton.tsx
@@ -37,7 +37,7 @@ const FacebookButton = ({ children }: prop) => {
       sx={{
         backgroundColor: "#E8F0F1",
         color: "black",
-        width: { md: "59%", xs: "100%" },
+        width: { md: "65%", xs: "100%" },
         height: "45px",
         fontSize: "1.2rem",
         fontFamily: "Arial",
