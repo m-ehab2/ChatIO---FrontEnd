e744d7c1573204a47aadb7dc3419bdf9596a651d
m-ehab2 <muhamedehab1@gmail.com>
2024-03-21 23:15:39 +0200

    feature : Hook to fetch one chat

---
+++ src/Hooks/useFetchOneChat.ts
@@ -0,0 +1,58 @@
+import { useState, useEffect } from "react";
+import axios, { AxiosError } from "axios";
+import { UserData } from "./useFetchAllUsers";
+axios.defaults.withCredentials = true;
+axios.defaults.withXSRFToken = true;
+
+export interface MessageData {
+  _id: string;
+  chat: string;
+  content: string;
+  createdAt: string;
+  seen: string[];
+  sender: UserData;
+}
+
+export interface OneChat {
+  chatImage: string;
+  chatName: string;
+  data: MessageData[];
+}
+
+interface ErrorResponse {
+  message: string;
+}
+const useFetchOneChat = (id: string) => {
+  const [chat, setChat] = useState<OneChat | null>(null);
+  const [error, setError] = useState<string | null>(null);
+  const [loading, setLoading] = useState(true);
+  const chatsURL = "http://localhost:8000/api/v1/chat/" + id;
+  const fetchChat = async () => {
+    try {
+      setChat(null);
+      const response = await axios.get(chatsURL, {
+        headers: {
+          "Access-Control-Allow-Origin": "http://127.0.0.1:5173",
+          "Content-Type": "application/json",
+        },
+      });
+      console.log(response.data);
+      setChat(response.data);
+      setLoading(false);
+    } catch (error) {
+      const errorResponse = error as AxiosError<ErrorResponse>;
+      setError(
+        errorResponse.response?.data.message ||
+          "An error occurred during Fetching Chats"
+      );
+      setLoading(false);
+    }
+  };
+  useEffect(() => {
+    fetchChat();
+  }, []);
+
+  return { error, chat, loading, fetchChat };
+};
+
+export default useFetchOneChat;
