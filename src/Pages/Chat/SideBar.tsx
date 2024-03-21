240699595dfba7cf4e549cb610cc673173d34efe
m-ehab2 <muhamedehab1@gmail.com>
2024-03-21 23:18:02 +0200

    modify : removed dummy data and utilized props

--- src/Pages/Chat/SideBar.tsx
+++ src/Pages/Chat/SideBar.tsx
@@ -1,57 +1,17 @@
 import { Box, Divider } from "@mui/material";
 import Profile from "../../components/Chat/SideBar/Profile";
 import Chats from "../../components/Chat/SideBar/Chats";
+import { ChatData } from "../../Hooks/useFetchAllChats";
+import { UserData } from "../../Hooks/useFetchAllUsers";
+import { useParams } from "react-router-dom";
 
-export default function SideBar() {
-  const chats = [
-    {
-      _id: "60e7c71d4cb96b001cf25441",
-      chatName: "Group Chat 1",
-      groupAdmin: "60e7c6e64cb96b001cf2543f",
-      users: [
-        {
-          _id: "60e7c6e64cb96b001cf2543f",
-          name: "Alice",
-          image: "https://example.com/avatar1.jpg",
-        },
-        {
-          _id: "60e7c6f64cb96b001cf25440",
-          name: "Bob",
-          image: "https://example.com/avatar2.jpg",
-        },
-        {
-          _id: "60e7c6f64cb96b001cf25440",
-          name: "Ehab",
-          image: "https://example.com/avatar2.jpg",
-        },
-      ],
-      lastMessage: {
-        _id: "60e7c71d4cb96b001cf25445",
-        text: "Hello, everyone!",
-      },
-    },
-    {
-      _id: "60e7c7284cb96b001cf25446",
-      chatName: "Individual Chat",
-      groupAdmin: "60e7c6e64cb96b001cf2543f",
-      users: [
-        {
-          _id: "60e7c6e64cb96b001cf2543f",
-          name: "Crapy",
-          image: "https://example.com/avatar1.jpg",
-        },
-        {
-          _id: "60e7c6f64cb96b001cf25441",
-          name: "Charlie",
-          image: "https://example.com/avatar3.jpg",
-        },
-      ],
-      lastMessage: {
-        _id: "60e7c7284cb96b001cf25447",
-        text: "Hi there!",
-      },
-    },
-  ];
+export interface SideBarProps {
+  chats: ChatData[];
+  groupedChats: ChatData[];
+  users: UserData[];
+}
+
+export default function SideBar({ chats, groupedChats, users }: SideBarProps) {
   return (
     <Box
       sx={{
@@ -63,7 +23,7 @@ export default function SideBar() {
     >
       <Profile />
       <Divider />
-      <Chats chats={chats} />
+      <Chats chats={chats} groupedChats={groupedChats} users={users} />
     </Box>
   );
 }
