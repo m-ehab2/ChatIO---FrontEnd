4ddd333f69c5e7ca85d8b73e41f98095467e56c1
Hassanmo99 <104209403+Hassanmo99@users.noreply.github.com>
2024-03-21 02:00:08 +0200

    modify: input component styling

--- src/components/MyInput.tsx
+++ src/components/MyInput.tsx
@@ -1,9 +1,9 @@
 import TextField from "@mui/material/TextField";
-import Box from "@mui/material/Box";
+// import Box from "@mui/material/Box";
+import Registration from "../Pages/Register/Registration";
 interface prop {
   type: "string" | "number" | "password" | "email" | "date";
   name: string;
-  placeholder: string;
 }
 
 // // const Input = ({ type, name, placeholder }: prop) => {
@@ -18,21 +18,20 @@ interface prop {
 
 // export default input;
 
-const MyInput = ({ type, name, placeholder }: prop) => {
+const MyInput = ({ type, name }: prop) => {
   return (
     <TextField
       sx={{
-        width: { md: "59%", xs: "100%" },
+        width: { md: "65%", xs: "100%" },
         borderRadius: "40px",
-        // maxHeight: "200px",
-        minWidth: "30px",
-        minHeight: "30px",
       }}
+      size="small"
       type={type}
-      id="outlined-basic"
+      id="outlined-error-helper-text"
       label={name}
       variant="outlined"
-      defaultValue={placeholder}
+      required
+      autoComplete="false"
     />
     // <TextField id="outlined-basic" label="Outlined" variant="outlined" />
     // <TextField id="outlined-basic" label="Outlined" variant="outlined" />
