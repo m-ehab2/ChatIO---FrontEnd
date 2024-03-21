ece348d72e5a761720e16a635b6211a8c6ea32b0
m-ehab2 <muhamedehab1@gmail.com>
2024-03-21 23:17:27 +0200

    modify : added hooks for data fetching

--- src/Pages/Chat/Chat.tsx
+++ src/Pages/Chat/Chat.tsx
@@ -1,8 +1,14 @@
 import { Box, Grid } from "@mui/material";
 import SideBar from "./SideBar";
 import MainChat from "./MainChat";
+import useFetchAllChats from "../../Hooks/useFetchAllChats";
+import useFetchAllGroupChats from "../../Hooks/useFetchAllGroupChats";
+import useFetchAllUsers from "../../Hooks/useFetchAllUsers";
 
 export default function Chat() {
+  const { chats } = useFetchAllChats();
+  const { chats: groupedChats } = useFetchAllGroupChats();
+  const { users } = useFetchAllUsers();
   return (
     <Box sx={{ background: "#F5F5F5", height: "100vh" }}>
       <Grid container sx={{ height: "100%" }}>
@@ -13,12 +19,13 @@ export default function Chat() {
             padding: "30px",
             border: "2px solid #DFE0E4",
             borderWidth: "0px 2px 0px 0px",
+            height: "100%",
           }}
         >
-          <SideBar />
+          <SideBar chats={chats} groupedChats={groupedChats} users={users} />
         </Grid>
-        <Grid item md={9}>
-          <MainChat />
+        <Grid sx={{ height: "100%" }} item md={9}>
+          <MainChat chats={chats} groupedChats={groupedChats} users={users} />
         </Grid>
       </Grid>
     </Box>
