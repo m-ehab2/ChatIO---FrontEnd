438cb386fd2375404e59ffd88d9df07644ceff17
Hassanmo99 <104209403+Hassanmo99@users.noreply.github.com>
2024-03-21 14:46:25 +0200

    modify: removed max function from password validation

--- src/Validations/UserSchema.ts
+++ src/Validations/UserSchema.ts
@@ -4,7 +4,7 @@ import * as yup from "yup";
 export const userSchema = yup.object().shape({
     userName: yup.string().required("Please Enter your Username"),
     email: yup.string().email("Invalid email").required("Please Enter your email"),
-    password:yup.string().min(8).max(12).required("Please Enter a password"),
+    password:yup.string().min(8).required("Please Enter a password"),
     confirmPassword:yup.string().oneOf([yup.ref("password")],"Password not matched").min(8).max(12).required()
     
 })
