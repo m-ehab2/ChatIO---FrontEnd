0cafca16736e8df43d614e13327b383c33a2bdd3
Hassanmo99 <104209403+Hassanmo99@users.noreply.github.com>
2024-03-22 17:30:56 +0200

    modify:changed name of a property

--- src/Validations/UserSchema.ts
+++ src/Validations/UserSchema.ts
@@ -5,7 +5,7 @@ export const userSchema = yup.object().shape({
     userName: yup.string().required("Please Enter your Username"),
     email: yup.string().email("Invalid email").required("Please Enter your email"),
     password:yup.string().min(8).required("Please Enter a password"),
-    confirmPassword:yup.string().oneOf([yup.ref("password")],"Password not matched").min(8).max(12).required()
+    passwordConfirm:yup.string().oneOf([yup.ref("password")],"Password not matched").min(8).max(12).required()
     
 })
     
