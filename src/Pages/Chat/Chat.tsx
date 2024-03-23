e6c1fffe45ffca14dbdb85331c077a5d6653fe87
m-ehab2 <muhamedehab1@gmail.com>
2024-03-23 07:37:36 +0200

    modify : moved the navBar to a side Bar

--- src/Pages/Chat/Chat.tsx
+++ src/Pages/Chat/Chat.tsx
@@ -1,31 +1,78 @@
-import { Box, Grid } from "@mui/material";
+import { Box, Button, Drawer, Grid, IconButton } from "@mui/material";
 import SideBar from "./SideBar";
 import MainChat from "./MainChat";
 import useFetchAllChats from "../../Hooks/useFetchAllChats";
 import useFetchAllGroupChats from "../../Hooks/useFetchAllGroupChats";
 import useFetchAllUsers from "../../Hooks/useFetchAllUsers";
 import withProtectedRoute from "../../components/HOC/withProtectedRoute";
-
+import { useState } from "react";
+import MenuIcon from "@mui/icons-material/Menu";
 export function Chat() {
   const { chats } = useFetchAllChats();
   const { chats: groupedChats } = useFetchAllGroupChats();
   const { users } = useFetchAllUsers();
+  const [open, setOpen] = useState(false);
+
+  const toggleDrawer = (newOpen: boolean) => () => {
+    setOpen(newOpen);
+  };
   return (
     <Box sx={{ background: "#F5F5F5", height: "100vh" }}>
       <Grid container sx={{ height: "100%" }}>
-        <Grid
-          item
-          md={3}
-          sx={{
-            padding: "30px",
-            border: "2px solid #DFE0E4",
-            borderWidth: "0px 2px 0px 0px",
-            height: "100%",
-          }}
+        {!open && (
+          <Button
+            variant="contained"
+            sx={{
+              display: { md: "block", xs: "none" },
+              position: "absolute",
+              right: "10px",
+              top: "10px",
+              fontSize: "14px",
+              fontFamily: "Inter",
+              color: "#f5f5f5",
+              backgroundColor: "#385A64",
+              padding: "7px",
+              "&:hover": {
+                color: "#f5f5f5",
+                backgroundColor: "#284A54",
+              },
+            }}
+            onClick={toggleDrawer(true)}
+          >
+            Open Nav
+          </Button>
+        )}
+        {!open && (
+          <IconButton
+            sx={{
+              display: { md: "none", xs: "block" },
+              position: "absolute",
+              right: "10px",
+              top: "10px",
+              borderRadius: "50%",
+              fontSize: "20px",
+              color: "#f5f5f5",
+              backgroundColor: "#385A64",
+              padding: "7px",
+              "&:hover": {
+                color: "#f5f5f5",
+                backgroundColor: "#284A54",
+              },
+            }}
+            onClick={toggleDrawer(true)}
+            aria-label="nav"
+          >
+            <MenuIcon />
+          </IconButton>
+        )}
+        <Drawer
+          open={open}
+          onClose={toggleDrawer(false)}
+          sx={{ width: { md: "1%", xs: "70%" } }}
         >
           <SideBar chats={chats} groupedChats={groupedChats} users={users} />
-        </Grid>
-        <Grid sx={{ height: "100%" }} item md={9}>
+        </Drawer>{" "}
+        <Grid sx={{ height: "100%" }} item xs={12}>
           <MainChat />
         </Grid>
       </Grid>
